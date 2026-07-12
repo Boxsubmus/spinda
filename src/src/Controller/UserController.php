cfc78d276353b20b82da9d513dec1a116e90e7b0
Braedon Vale <pelly@boxsubmus.com>
2026-07-12 14:21:35 -0400

    things

--- src/src/Controller/UserController.php
+++ src/src/Controller/UserController.php
@@ -2,23 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\BeatmapsetRepository;
 use App\Repository\UserRepository;
+use App\Service\StorageService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
+    public function __construct(
+        private readonly StorageService $storage,
+    ) {}
+
     #[Route('/users/{id}', name: 'app_user')]
-    public function index($id, UserRepository $repository): Response
+    public function index($id, UserRepository $repository, BeatmapsetRepository $beatmapsRe): Response
     {
         $user = $repository->findOneBy([
             'id' => $id
         ]);
+        
+        $beatmaps = $beatmapsRe->findBy([
+            'author' => $id
+        ]);
 
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-            'user' => $user
+            'storage' => $this->storage,
+            'user' => $user,
+            'beatmaps' => $beatmaps
         ]);
     }
 }
